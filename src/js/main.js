4d7ede79736dcc591f59a1a35a1124beef54e2f7
Przemek Juzwiszin <przemekjuzwiszin@gmail.com>
2021-04-11 23:16:08 +0200

    format JS document

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,21 @@
 
 // service worker registration - remove if you're not going to use it
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("serviceworker.js").then(
+      function (registration) {
+        // Registration was successful
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      function (err) {
+        // registration failed :(
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
   });
 }
 
@@ -24,19 +30,19 @@ var NANONAUT_HEIGHT = 229;
 var GROUND_Y = 540;
 
 //PRECONFIGURATION
-var canvas = document.createElement('canvas');
-var c = canvas.getContext('2d');
+var canvas = document.createElement("canvas");
+var c = canvas.getContext("2d");
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 document.body.appendChild(canvas);
 
 var nanonautImage = new Image();
-nanonautImage.src = 'assets/img/Nanonaut.png';
+nanonautImage.src = "assets/img/Nanonaut.png";
 
 var nanonautX = 50;
 var nanonautY = 40;
 
-window.addEventListener('load', start);
+window.addEventListener("load", start);
 
 function start() {
   window.requestAnimationFrame(mainLoop);
@@ -51,9 +57,7 @@ function mainLoop() {
 
 //CONTROL
 //UPDATE
-function update() {
-  
-}
+function update() {}
 
 //DRAWING
 function draw() {
