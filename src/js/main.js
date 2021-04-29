a3ddc7134d266836633b6e22206adb531fdc382c
Przemek Juzwiszin <przemekjuzwiszin@gmail.com>
2021-04-29 18:02:54 +0200

    format JS document

--- src/js/main.js
+++ src/js/main.js
@@ -188,11 +188,10 @@ function update() {
 }
 
 function updateRobots() {
-
   //Moving and animating robots
   for (var i = 0; i < robotData.length; i++) {
     robotData[i].x -= ROBOT_X_SPEED;
-    if ((gameFrameCounter % ROBOT_ANIMATION_SPEED) === 0) {
+    if (gameFrameCounter % ROBOT_ANIMATION_SPEED === 0) {
       robotData[i].frameNr = robotData[i].frameNr + 1;
       if (robotData[i].frameNr >= ROBOT_NR_ANIMATION_FRAMES) {
         robotData[i].frameNr = 0;
